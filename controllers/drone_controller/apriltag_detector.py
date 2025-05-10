2b12c87b6800f421b46c7e5aedb768bf920a00a8
Evan <evan@brucemcrooster.dev>
2025-05-10 16:13:50 -0400

    🚧 Added function to get AprilTag relative offsets
    Signed-off-by: Evan <evan@brucemcrooster.dev>

--- controllers/drone_controller/apriltag_detector.py
+++ controllers/drone_controller/apriltag_detector.py
@@ -41,3 +41,44 @@ class AprilTagDetector:
         min_corners = corners[min_index]
 
         return int(ids[min_index]), min_corners
+
+
+    def _get_apriltag_orientation(self, image: np.ndarray, corners: np.ndarray) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]] | None:
+        """
+        Gets the location and rotation of the given AprilTag in the image, relative to the camera.
+        :param image: The image that contains the AprilTag.
+        :param corners: The corners of the AprilTag, relative to the image frame.
+        :return: If no AprilTag detected, returns None. Otherwise, returns the position and rotation of the lowest-ID AprilTag
+        in the coordinate space of the camera. The rotation vector is a Rodrigues rotation vector.
+        """
+
+        # Camera, at least on this simulator Crazyflie model, does not have a focal length set.
+        # Thus, camera.getFocalLength() would return 0 and mess up calculations.
+        # Instead, we compute these parameters using the width/height and FOV.
+        # This does assume the FOV is the same for the width and height (the camera takes a square image).
+        fx = (self.camera.width / 2) / (self.camera.fov / 2)
+        fy = (self.camera.height / 2) / (self.camera.fov / 2)
+
+        # Center of focus is the center of the camera view
+        cx = self.camera.width / 2
+        cy = self.camera.height / 2
+
+        camera_matrix = np.array([[fx, 0, cx],
+                                  [0, fy, cy],
+                                  [0, 0, 1]], dtype=np.float32)
+
+        marker_length = 0.1
+        obj_points = np.array([[-marker_length / 2.0, marker_length / 2.0, 0],
+                               [marker_length / 2.0, marker_length / 2.0, 0],
+                               [marker_length / 2.0, -marker_length / 2.0, 0],
+                               [-marker_length / 2.0, -marker_length / 2.0, 0]],
+                              dtype=np.float32)
+
+        _, rvec, tvec = cv2.solvePnP(
+            objectPoints=obj_points,
+            imagePoints=corners,
+            cameraMatrix=camera_matrix,
+            distCoeffs=None
+        )
+
+        return rvec, tvec
