8187c08bcf0998fc0c670fce642b0f3b1827ec77
Evan <evan@brucemcrooster.dev>
2025-05-10 15:59:20 -0400

    🚧 Add method to get image from camera
    Signed-off-by: Evan <evan@brucemcrooster.dev>

--- controllers/drone_controller/apriltag_detector.py
+++ controllers/drone_controller/apriltag_detector.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy as np
+
 from controller import Camera
 
 
@@ -5,3 +8,12 @@ class AprilTagDetector:
     def __init__(self, camera: Camera):
         self.camera = camera
 
+
+    def _get_camera_image(self) -> np.ndarray:
+        """
+        Gets the camera image representation in BGR format.
+        :arg cam: The Camera to get the image from.
+        :returns: The image from the camera, in BGR format.
+        """
+        img = np.frombuffer(self.camera.getImage(), dtype=np.uint8).reshape((self.camera.height, self.camera.width, 4))
+        return cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
