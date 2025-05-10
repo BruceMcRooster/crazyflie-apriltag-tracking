89497eec974d5a7088c6c39ee6d83809e50d09fc
Evan <evan@brucemcrooster.dev>
2025-05-10 09:26:18 -0400

    🚨 Add typing to drone_controller devices
    Signed-off-by: Evan <evan@brucemcrooster.dev>

--- controllers/drone_controller/drone_controller.py
+++ controllers/drone_controller/drone_controller.py
@@ -19,8 +19,10 @@ Controls the crazyflie and implements a wall following method in webots in Pytho
 Author:   Kimberly McGuire (Bitcraze AB)
 """
 
+from typing import cast
 
 from controller import Robot
+from controller import Motor, InertialUnit, GPS, Gyro, Camera, DistanceSensor
 from controller import Keyboard
 
 from math import cos, sin
@@ -35,35 +37,35 @@ if __name__ == '__main__':
     timestep = int(robot.getBasicTimeStep())
 
     # Initialize motors
-    m1_motor = robot.getDevice("m1_motor")
+    m1_motor = cast(Motor, robot.getDevice("m1_motor"))
     m1_motor.setPosition(float('inf'))
     m1_motor.setVelocity(-1)
-    m2_motor = robot.getDevice("m2_motor")
+    m2_motor = cast(Motor, robot.getDevice("m2_motor"))
     m2_motor.setPosition(float('inf'))
     m2_motor.setVelocity(1)
-    m3_motor = robot.getDevice("m3_motor")
+    m3_motor = cast(Motor, robot.getDevice("m3_motor"))
     m3_motor.setPosition(float('inf'))
     m3_motor.setVelocity(-1)
-    m4_motor = robot.getDevice("m4_motor")
+    m4_motor = cast(Motor, robot.getDevice("m4_motor"))
     m4_motor.setPosition(float('inf'))
     m4_motor.setVelocity(1)
 
     # Initialize Sensors
-    imu = robot.getDevice("inertial_unit")
+    imu = cast(InertialUnit, robot.getDevice("inertial_unit"))
     imu.enable(timestep)
-    gps = robot.getDevice("gps")
+    gps = cast(GPS, robot.getDevice("gps"))
     gps.enable(timestep)
-    gyro = robot.getDevice("gyro")
+    gyro = cast(Gyro, robot.getDevice("gyro"))
     gyro.enable(timestep)
-    camera = robot.getDevice("camera")
+    camera = cast(Camera, robot.getDevice("camera"))
     camera.enable(timestep)
-    range_front = robot.getDevice("range_front")
+    range_front = cast(DistanceSensor, robot.getDevice("range_front"))
     range_front.enable(timestep)
-    range_left = robot.getDevice("range_left")
+    range_left = cast(DistanceSensor, robot.getDevice("range_left"))
     range_left.enable(timestep)
-    range_back = robot.getDevice("range_back")
+    range_back = cast(DistanceSensor, robot.getDevice("range_back"))
     range_back.enable(timestep)
-    range_right = robot.getDevice("range_right")
+    range_right = cast(DistanceSensor, robot.getDevice("range_right"))
     range_right.enable(timestep)
 
     # Get keyboard
