258ca01b9a936bc7b5114d0e01bc485b3e9da9d5
Evan <evan@brucemcrooster.dev>
2025-05-10 18:35:09 -0400

    🚧 Add function to get min-id AprilTag
    Signed-off-by: Evan <evan@brucemcrooster.dev>

--- controllers/drone_controller/apriltag_detector.py
+++ controllers/drone_controller/apriltag_detector.py
@@ -1,4 +1,7 @@
+from typing import Tuple
+
 import cv2
+from cv2 import aruco as aruco
 import numpy as np
 
 from controller import Camera
@@ -6,6 +9,9 @@ from controller import Camera
 
 class AprilTagDetector:
     def __init__(self, camera: Camera):
+        apriltag_dict = aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_16h5)
+        parameters = aruco.DetectorParameters()
+        self.detector = aruco.ArucoDetector(apriltag_dict, parameters)
         self.camera = camera
 
 
@@ -17,3 +23,21 @@ class AprilTagDetector:
         """
         img = np.frombuffer(self.camera.getImage(), dtype=np.uint8).reshape((self.camera.height, self.camera.width, 4))
         return cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+
+    def _get_min_id_apriltag(self, image: np.ndarray) -> Tuple[int, np.ndarray] | None:
+        """
+        Gets the id and corners of the lowest-ID AprilTag in the image.
+        :param image: The image to process for AprilTags.
+        :return: A tuple of the id and corners of the lowest-ID AprilTag, or None if no AprilTag found.
+        """
+        corners, ids, rejected_image_points = self.detector.detectMarkers(image)
+
+        if ids is None or len(ids) == 0:
+            return None
+
+        min_index = int(np.argmin(ids))
+
+        min_corners = corners[min_index]
+
+        return int(ids[min_index]), min_corners
